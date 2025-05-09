30fc369e0b699ac4b2722e3b641351ee8778260b
Shrikant Vishwakarma <64365738+StrixV54@users.noreply.github.com>
2025-05-09 17:01:53 +0530

    fix(cors): add frontend url

--- backend/server.ts
+++ backend/server.ts
@@ -12,11 +12,11 @@ const PORT = process.env.PORT || 4000;
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173"]
+        origin: ["http://localhost:5173", "https://kaznote-wss.vercel.app/"]
     }
 });
 
-app.use(cors());
+app.use(cors({ origin: ["*"] }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
